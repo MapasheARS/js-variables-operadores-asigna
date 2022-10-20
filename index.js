dbff971c7b49785b43a763e1f27a6803a6896144
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-19 19:30:31 -0500

    Actualizaicon, operadores asignados

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ let precioManzanas = 5;
 let precioPlatanos = 6;
 
 //utiliza el operador += para incrementar numManzanas por 5
+numManzanas += 5
 
 //utiliza el operador -= para disminuir el numPlatanos por 2
-
+numPlatanos -= 2
 //utiliza el operador *= para duplicar el precio de las Manzanas
-
+precioManzanas *= 2
 //utiliza el operador /= para dividir el precio de los platanos entre 2
+precioPlatanos /= 2
 
 // totalManzanas = precio de las manzanas multiplicado por el numero de manzanas.
+totalManzanas = precioManzanas * numManzanas
 
 //totalPlatanos = precio de platanos multiplicado por el numero de platanos.
+totalPlatanos = precioPlatanos * numPlatanos
